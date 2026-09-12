51a45db71235ab8129edc6f7f533074829a23061
mahbubur75 <mahbubur23212830@gmail.com>
2026-09-12 19:38:26 +0600

    Update project

---
+++ src/Components/ToastContainer.tsx
@@ -0,0 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Toast = () => {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      pauseOnHover
+    />
+  );
+};
+
+export default Toast;
